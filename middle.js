33ffa01ebdef646ae2e970aa3c85a5d6de47f91d
Kaiya Hedley <keshedley@gmail.com>
2022-07-27 15:22:25 -0600

    finished middle

--- middle.js
+++ middle.js
@@ -1,21 +1,41 @@
-let compare = ""
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
   for (let x = 0; x < arr1.length; x++) {
   if (arr1[x] !== arr2[x]) {
-    compare = false
-} else if (arr1[x] === arr2[x]) {
-    compare = true
+    return false 
   }
-  }
-  return compare
 }
+  return true
+  }
 
-  const assertArraysEqual = function() {
-    assertEqual(compare, true)
-    if (compare === false) {
-      equal = console.log('Arrays are not equal.')
-    } else if (compare === true) {
-      equal = console.log('Arrays are equal!')
-    }
-    return equal
-    }
+  const assertArraysEqual = function(actual, expected) {
+      if (eqArrays(actual, expected)) {
+        console.log(`🖤 Assertion Passed: ${actual} === ${expected}`);
+        } else {
+        console.log(`💔 Assertion Failed: ${actual} !== ${expected}`);
+        }
+      };
+
+  const middle = function(arr) {
+    const mid = Math.floor(arr.length / 2)
+    result = []
+      if (arr.length <= 2) {
+        return result
+      } else if (arr.length % 2 !== 0) {
+        result = [arr[mid]]
+      } else {
+        result = arr.slice(arr[mid -2], arr[mid])
+      }
+      return result
+      }
+
+  assertArraysEqual(middle([1]), []) // => []
+  assertArraysEqual(middle([1, 2]), []) // => []
+
+  assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
+  assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
+
+  assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
